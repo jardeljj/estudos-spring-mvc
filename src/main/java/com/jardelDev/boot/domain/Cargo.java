9a92f5a7fd48cc843440d36815d4d1223c7dbd75
Jardel Junior <55859324+jardeljj@users.noreply.github.com>
2025-02-10 13:25:36 -0300

    Criando entidade funcionario

--- src/main/java/com/jardelDev/boot/domain/Cargo.java
+++ src/main/java/com/jardelDev/boot/domain/Cargo.java
@@ -2,6 +2,8 @@ package com.jardelDev.boot.domain;
 
 import jakarta.persistence.*;
 
+import java.util.List;
+
 @Entity
 @Table(name = "CARGOS")
 public class Cargo extends AbstractEntity<Long>{
@@ -13,11 +15,30 @@ public class Cargo extends AbstractEntity<Long>{
     @JoinColumn(name = "id_departamento_fk")
     private Departamento departamento;
 
+    @OneToMany(mappedBy = "cargo")
+    private List<Funcionario> funcionarios;
+
     public String getNome() {
         return nome;
     }
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    public Departamento getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(Departamento departamento) {
+        this.departamento = departamento;
+    }
+
+    public List<Funcionario> getFuncionarios() {
+        return funcionarios;
+    }
+
+    public void setFuncionarios(List<Funcionario> funcionarios) {
+        this.funcionarios = funcionarios;
     }
 }
