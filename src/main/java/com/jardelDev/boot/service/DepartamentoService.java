5dac02c5bf1ddd3370d5833f2a3aacf70847e8bc
Jardel Junior <55859324+jardeljj@users.noreply.github.com>
2025-02-11 16:12:02 -0300

    Finalizando services.

--- src/main/java/com/jardelDev/boot/service/DepartamentoService.java
+++ src/main/java/com/jardelDev/boot/service/DepartamentoService.java
@@ -1,4 +1,20 @@
 package com.jardelDev.boot.service;
 
+import com.jardelDev.boot.domain.Cargo;
+import com.jardelDev.boot.domain.Departamento;
+
+import java.util.List;
+
 public interface DepartamentoService {
+
+    void salvar(Departamento departamento);
+
+    void editar(Departamento departamento);
+
+    void excluir(Long id);
+
+    Departamento buscarPorId(Long id);
+
+    List<Departamento> buscarTodos();
+
 }
--- src/main/java/com/jardelDev/boot/service/FuncionarioService.java
+++ src/main/java/com/jardelDev/boot/service/FuncionarioService.java
@@ -1,4 +1,20 @@
 package com.jardelDev.boot.service;
 
+import com.jardelDev.boot.domain.Funcionario;
+
+import java.util.List;
+
 public interface FuncionarioService {
+
+    void salvar(Funcionario funcionario);
+
+    void editar(Funcionario funcionario);
+
+    void excluir(Long id);
+
+    Funcionario buscarPorId(Long id);
+
+    List<Funcionario> buscarTodos();
+
+
 }
