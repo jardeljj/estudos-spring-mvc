f5f9be22ea683bc3b1a0369b1575d5d38d4653d9
Jardel Junior <55859324+jardeljj@users.noreply.github.com>
2025-02-13 14:04:16 -0300

    implementando a funcionalidade de salvar para departamentos

--- src/main/java/com/jardelDev/boot/web/controller/DepartamentoController.java
+++ src/main/java/com/jardelDev/boot/web/controller/DepartamentoController.java
@@ -1,15 +1,23 @@
 package com.jardelDev.boot.web.controller;
 
+import com.jardelDev.boot.domain.Departamento;
+import com.jardelDev.boot.service.DepartamentoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/departamentos")
 public class DepartamentoController {
 
+    @Autowired
+    private DepartamentoService service;
+
+
     @GetMapping("/cadastrar")
-    public String cadastrar(){
+    public String cadastrar(Departamento departamento){
         return "/departamento/cadastro";
     }
 
@@ -17,4 +25,11 @@ public class DepartamentoController {
     public String listar(){
         return "/departamento/lista";
     }
+
+    @PostMapping("/salvar")
+    public String salvar(Departamento departamento){
+        service.salvar(departamento);
+        return "redirect:/departamentos/cadastrar";
+    }
+
 }
