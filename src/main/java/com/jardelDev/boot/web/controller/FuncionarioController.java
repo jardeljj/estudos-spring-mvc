076d89d01842c5c528aef08cef775d2ced862b54
Jardel Junior <55859324+jardeljj@users.noreply.github.com>
2025-02-19 01:10:01 -0300

    Implementado a lista de funcionarios, edit e delete

--- src/main/java/com/jardelDev/boot/web/controller/FuncionarioController.java
+++ src/main/java/com/jardelDev/boot/web/controller/FuncionarioController.java
@@ -7,10 +7,8 @@ import com.jardelDev.boot.service.CargoService;
 import com.jardelDev.boot.service.FuncionarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
@@ -32,7 +30,8 @@ public class FuncionarioController {
     }
 
     @GetMapping("/listar")
-    public String listar(){
+    public String listar(ModelMap model){
+        model.addAttribute("funcionarios", funcionarioService.buscarTodos());
         return "/funcionario/lista";
     }
 
@@ -41,6 +40,26 @@ public class FuncionarioController {
         funcionarioService.salvar(funcionario);
         attr.addFlashAttribute("success", "Funcionário inserido com sucesso.");
         return "redirect:/funcionarios/cadastrar";
+    }
+
+    @GetMapping("/editar/{id}")
+    public String preEditar(@PathVariable("id") Long id, ModelMap model) {
+        model.addAttribute("funcionario", funcionarioService.buscarPorId(id));
+        return "funcionario/cadastro";
+    }
+
+    @PostMapping("/editar")
+    public String editar(Funcionario funcionario, RedirectAttributes attr) {
+        funcionarioService.editar(funcionario);
+        attr.addFlashAttribute("success", "Funcionário editado com sucesso.");
+        return "redirect:/funcionarios/cadastrar";
+    }
+
+    @GetMapping("/excluir/{id}")
+    public String excluir(@PathVariable("id") Long id, RedirectAttributes attr) {
+        funcionarioService.excluir(id);
+        attr.addFlashAttribute("success", "Funcionário removido com sucesso.");
+        return "redirect:/funcionarios/listar";
     }
 
     @ModelAttribute("cargos")
