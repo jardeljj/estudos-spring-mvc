26e355dfb2716833cad3986c4d0b9bd95586080c
Jardel Junior <55859324+jardeljj@users.noreply.github.com>
2025-02-20 23:52:06 -0300

    validando funcionario e endereço

--- src/main/java/com/jardelDev/boot/domain/Funcionario.java
+++ src/main/java/com/jardelDev/boot/domain/Funcionario.java
@@ -1,6 +1,11 @@
 package com.jardelDev.boot.domain;
 
 import jakarta.persistence.*;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Size;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.NumberFormat;
 
@@ -11,25 +16,32 @@ import java.time.LocalDate;
 @Table(name = "FUNCIONARIOS")
 public class Funcionario extends AbstractEntity<Long>{
 
+    @NotBlank
+    @Size(max = 255, min = 3)
     @Column(nullable = false, unique = true)
     private String nome;
 
+    @NotNull
     @NumberFormat(style = NumberFormat.Style.CURRENCY, pattern = "#,##0.00")
     @Column(nullable = false, columnDefinition = "DECIMAL(7,2) DEFAULT 0.00")
     private BigDecimal salario;
 
+    @NotNull
+    @PastOrPresent(message = "{PastOrPresent.funcionario.dataEntrada}")
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
-    @Column(name = "data_entrada",nullable = false, columnDefinition = "DATE")
+    @Column(name= "data_entrada", nullable = false, columnDefinition = "DATE")
     private LocalDate dataEntrada;
 
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
-    @Column(name = "data_saida",nullable = true, columnDefinition = "DATE")
+    @Column(name = "data_saida", columnDefinition = "DATE")
     private LocalDate dataSaida;
 
+    @Valid
     @OneToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "endereco_id_fk")
     private Endereco endereco;
 
+    @NotNull(message = "{NotNull.funcionario.cargo}")
     @ManyToOne
     @JoinColumn(name = "cargo_id_fk")
     private Cargo cargo;
