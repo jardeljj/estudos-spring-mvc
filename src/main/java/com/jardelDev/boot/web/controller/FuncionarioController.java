6051217068df5e1e074c8748148800d8dcb5bd83
Jardel Junior <55859324+jardeljj@users.noreply.github.com>
2025-02-23 01:21:48 -0300

    Validação de data com spring validator

--- src/main/java/com/jardelDev/boot/web/controller/FuncionarioController.java
+++ src/main/java/com/jardelDev/boot/web/controller/FuncionarioController.java
@@ -5,12 +5,14 @@ import com.jardelDev.boot.domain.Funcionario;
 import com.jardelDev.boot.domain.UF;
 import com.jardelDev.boot.service.CargoService;
 import com.jardelDev.boot.service.FuncionarioService;
+import com.jardelDev.boot.web.controller.validator.FuncionarioValidator;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -27,6 +29,11 @@ public class FuncionarioController {
 
     @Autowired
     private CargoService cargoService;
+
+    @InitBinder
+    public void initBinder(WebDataBinder binder){
+        binder.addValidators(new FuncionarioValidator());
+    }
 
     @GetMapping("/cadastrar")
     public String cadastrar(Funcionario funcionario) {
