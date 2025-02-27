0ad0e66b269f53544fcd27059fa0511df9a96823
Jardel Junior <55859324+jardeljj@users.noreply.github.com>
2025-02-27 14:52:44 -0300

    preparando consulta paginada

--- src/main/java/com/jardelDev/boot/dao/CargoDaoImpl.java
+++ src/main/java/com/jardelDev/boot/dao/CargoDaoImpl.java
@@ -1,10 +1,35 @@
 package com.jardelDev.boot.dao;
 
 import com.jardelDev.boot.domain.Cargo;
+import com.jardelDev.boot.util.PaginacaoUtil;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class CargoDaoImpl extends AbstractDao<Cargo, Long> implements CargoDao {
 
+
+    public PaginacaoUtil<Cargo> buscaPaginada(int pagina){
+        int tamanho = 5;
+        int inicio = (pagina - 1) * tamanho;
+        List<Cargo> cargos = getEntityManager()
+                .createQuery("select c from Cargo c order by c.nome asc", Cargo.class)
+                .setFirstResult(inicio)
+                .setMaxResults(tamanho)
+                .getResultList();
+
+        Long totalRegistros = count();
+        Long totalDePaginas = (totalRegistros + (tamanho - 1 )) / tamanho;
+
+        return new PaginacaoUtil<>(tamanho, pagina, totalDePaginas, cargos);
+    }
+
+    public Long count(){
+
+        return getEntityManager()
+                .createQuery("select count(*) from Cargo", Long.class)
+                .getSingleResult();
+    }
 
 }
